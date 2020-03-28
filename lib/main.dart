7abca753fc10f38bf6fb0ec45b21e0ef01a52c4a
luccasoli <oliveira.mesquita@aluno.uece.br>
2020-03-28 00:07:04 -0300

    applying some styles, adding intl package dependency and adding a simple form

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/Transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main(List<String> args) {
   runApp(
@@ -17,13 +18,19 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
-  final List<Transaction> _transactions = [
+  final _transactions = [
     Transaction(
       id: '1',
-      title: 'Novo tênis de corrida',
+      title: 'Novo tênis de corrida dddd',
       value: 310.30,
       date: DateTime.now(),
-    )
+    ),
+    Transaction(
+      id: '2',
+      title: 'Conta de Luz',
+      value: 211,
+      date: DateTime.now(),
+    ),
   ];
 
   @override
@@ -32,24 +39,99 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Card(
-            child: Text('Gráfico'),
-          ),
-          Column(
-            children: _transactions
-                .map(
-                  (e) => Card(
-                    child: Text(
-                      e.title,
-                    ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            // Gráfico
+            Card(
+              child: Text('Gráfico'),
+            ),
+            // Lista de Despesas
+            Column(
+              children: _transactions.map((transaction) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      // Valor
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          'R\$ ${transaction.value.toStringAsFixed(2)}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              transaction.title,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Text(
+                              DateFormat('dd MMM y').format(transaction.date),
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                )
-                .toList(),
-          )
-        ],
+                );
+              }).toList(),
+            ),
+            Card(
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Título',
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Valor (R\$)',
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        FlatButton(
+                          child: Text('Nova Transação'),
+                          onPressed: () {},
+                          textColor: Colors.purple,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
