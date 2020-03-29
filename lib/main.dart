731312fa7fba41dc36285eb62aff1bf15fc4aa12
luccasoli <oliveira.mesquita@aluno.uece.br>
2020-03-28 21:37:50 -0300

    putting form inside a BottomSheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'dart:math';
+
 import 'package:expenses/components/transaction_form.dart';
 import 'package:expenses/components/transaction_list.dart';
-import 'package:expenses/components/transaction_user.dart';
 import 'package:expenses/models/Transaction.dart';
 import 'package:flutter/material.dart';
 
@@ -15,16 +16,67 @@ class ExpensesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Home(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final _transactions = [
+    Transaction(
+      id: '1',
+      title: 'Novo tênis de corrida dddd',
+      value: 310.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: '26',
+      title: 'Conta #06',
+      value: 211,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+
+    Navigator.of(context).pop();
+  }
+
+  void _openTransactionFormModal(context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return TransactionForm(_addTransaction);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _openTransactionFormModal(context),
+          ),
+        ],
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -34,9 +86,14 @@ class Home extends StatelessWidget {
             child: Text('Gráfico'),
           ),
           // Lista de Despesas
-          TransactionUser(),
+          TransactionList(_transactions),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _openTransactionFormModal(context),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
